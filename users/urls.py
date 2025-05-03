baf04e80e8886db39d3045cc6a6fc91ddd0a8755
oesara <osrosr888@naver.com>
2025-05-03 20:15:02 +0900

    feat: [users] 로그아웃 기능과 세션 유지 확인 기능 구현urls, views (#20)

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 인증 (Users)
+    # 인증
     path('signup/', views.signup, name='signup'), # POST : 회원가입
     path('login/', views.login, name='login'), # POST : 로그인
     path('logout/', views.logout, name='logout'), # POST : 로그아웃
@@ -11,10 +11,13 @@ urlpatterns = [
     path('social/extra-info/', views.social_extra_info, name='social_extra_info'), # POST : 소셜 로그인 유저의 추가 정보
     path('password/change/', views.password_change, name='password_change'), # POST : 비밀번호 변경
 
-    # 마이페이지 (Users)
+    # 마이페이지
     path('mypage/', views.mypage, name='mypage'), # GET : 마이페이지(프로필 일부, 분석내역 일부, 찜한 공모전 일부 확인)
     path('profile/', views.profile, name='profile'),  # PUT: 내 정보 수정, DELETE: 회원 탈퇴
     path('liked-contests/', views.liked_contests, name='liked_contests'), # GET : 내가 찜한 공모전 전체 조회
+
+    # 세션 유지(로그인 상태) 확인
+    path('session-check/', views.session_check, name='session_check'), # GET : 현재 사용자의 로그인 상태 확인 (프론트엔드에서 로그인 유지 여부 확인)
 ]
 
 """
