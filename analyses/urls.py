397165ac926895baf82770751b2d3c07ac1ec58d
juhee park <pjhch1009@gmail.com>
2025-05-15 18:04:51 +0900

    feat: [analysis] 통합 분석 내역 생성과 Fread 분석 생성 기능 구현 (#13, #21)

--- analyses/urls.py
+++ analyses/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import FreadAnalysisView
+from .views import analysis_view, spell_check_view
 from . import views
 
 urlpatterns = [
-    path('', AnalysisView.as_view(), name='통합 분석 결과(GET)'),
-    path('fread/', FreadAnalysisView.as_view(), name='fread 분석 요청(POST)'),
-    path('fread/<int:analysis_id>/', FreadAnalysisView.as_view(), name='fread 분석 결과(GET)'),
-    path('sentence/', views.sentence_analysis, name='문장 개선하기(POST)'),
-    path('sentence/<int:analysis_id>/', views.sentence_analysis, name='문장 개선 결과'),
-    path('spellcheck/', views.spellcheck, name='맞춤법 검사'),
+    path('', analysis_view.analysis, name='통합 분석 결과(GET)'),
+    path('fread/', analysis_view.as_view(), name='fread 분석 요청(POST)'),
+    path('fread/<int:analysis_id>/', analysis_view.as_view(), name='fread 분석 결과(GET)'),
+    # path('sentence/', views, name='문장 개선하기(POST)'),
+    # path('sentence/<int:analysis_id>/', views, name='문장 개선 결과'),
+    path('spellcheck/', spell_check_view.spellcheck, name='맞춤법 검사'),
 ]
 
