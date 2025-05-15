2b84b984e93745d0b5900aa083f68d024b0ad5c5
Joey <pjhch1009@gmail.com>
2025-05-15 22:38:57 +0900

    add: [analysis] 통합 분석 내역 조회, 프리드 분석 결과 조회 기능 추가 (#13, #21)

--- analyses/views/analysis_view.py
+++ analyses/views/analysis_view.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated  # 권한 클래스 (인증된 사용자, 관리자)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -11,46 +12,63 @@ from ..serializers import AnalysisCreateSerializer, AnalysisListSerializer, Frea
 from ..utils.generate_fread_analysis import generate_fread_analysis_score, generate_fread_ai_comments, generate_fread_solutions
 from ..utils.generate_analysis import generate_title_from_gpt
 
+
+
 # 통합 분석 내역 (GET)
+@ api_view(['GET'])
+@ permission_classes([IsAuthenticated]) # 로그인한 사용자만 사용 가능
 def analysis(request):
-    return
+    analyses = Analysis.objects.filter(user=request.user)
+    serializer = AnalysisListSerializer(analyses, many=True)
+    return Response(serializer.data)
+
+
 
 # 프리드 분석 (GET, POST)
 class FreadAnalysisView(APIView):
-    def get(self, request):
-        return
+    permission_classes = [IsAuthenticated]  # 로그인한 사용자만 사용 가능
+
+    # 프리드 분석 결과 요청 시
+    def get(self, request, analysis_id):
+        fread_analysis = FreadAnalysis.objects.get(pk=analysis_id)
+        serializer = FreadAnalysisSerializer(fread_analysis)
+        return Response(serializer.data)
     
+    # 프리드 분석 요청 시
     def post(self, request):
+        # 통합 분석내역 (analysis) 생성
         serializer = AnalysisCreateSerializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         analysis = serializer.save()  # serializer에서 Analysis 생성 (title 제외) (자동으로 user, analysis_type, title, original_text 포함)
+        # 사용자가 입력한 원본 텍스트
         original_text = analysis.original_text
 
-        # GPT 점수 데이터 생성
+
+        # GPT 점수 데이터 생성 (fread analysis)
         score_data = generate_fread_analysis_score(original_text)
-        
-        # GPT ai_comments 생성
-        ai_comments = generate_fread_ai_comments(original_text)
-
-        # GPT solution 생성
-        solutions_data = generate_fread_solutions(original_text)
-
         
         # 만약 gpt가 점수 데이터를 잘 못 생성했다면 (객체가 와야 하는데, 문자열(에러메시지)이 왔다면)
         if isinstance(score_data, str): 
             return Response({'error_message': score_data}, status=status.HTTP_400_BAD_REQUEST)  # 넘어온 문자열(에러메시지)을 클라에게 반환
 
-        # gpt가 점수 데이터를 잘 생성했다면, 아까 미뤄뒀던 analysis(통합 분석 내역)의 title 생성 (점수 데이터를 이용)
+        # gpt가 점수 데이터를 잘 생성했다면, 아까 미뤄뒀던 통합 분석 내역 (analysis)의 title 생성 (점수 데이터를 이용)
         title = generate_title_from_gpt(original_text[:300], score_data)
         
         # Analysis 객체의 제목 업데이트
         analysis.title = title
-        analysis.save()  # 변경 사항 저장
+        analysis.save()  # 최종 저장
 
+
+        # GPT ai_comments 생성 (fread analysis)
+        ai_comments = generate_fread_ai_comments(original_text)
 
         # 만약 gpt가 ai_comments 데이터를 잘 못 생성했다면 (딕셔너리가 와야 하는데, 문자열(에러메시지)이 왔다면)
         if isinstance(ai_comments, str): 
             return Response({'error_message': ai_comments}, status=status.HTTP_400_BAD_REQUEST)  # 넘어온 문자열(에러메시지)을 클라에게 반환
+
+
+        # GPT solution 생성 (fread analysis)
+        solutions_data = generate_fread_solutions(original_text)
         
         # 만약 gpt가 solution 데이터를 잘 못 생성했다면 (딕셔너리가 와야 하는데, 문자열(에러메시지)이 왔다면)
         if isinstance(solutions_data, str): 
@@ -58,7 +76,7 @@ class FreadAnalysisView(APIView):
         
 
 
-        # 모든 데이터가 잘 생성됐다면
+        # 모든 데이터가 잘 생성됐다면 (fread analysis)
         FreadAnalysis.objects.create(
             analysis = analysis,  # OneToOneField 연결
             total = score_data.total,
